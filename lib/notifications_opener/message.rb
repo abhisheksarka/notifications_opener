407a7c30c46aa1a92c1aeb1571ebf030061a081a
abhisheksarka <abhisheksarka@gmail.com>
2016-08-07 17:37:13 +0530

    added ERB for file rendering

--- lib/notifications_opener/message.rb
+++ lib/notifications_opener/message.rb
@@ -1,24 +1,30 @@
+require 'erb'
+
 module NotificationsOpener
   class Message
-    attr_accessor :config
+    attr_accessor :config,
+                  :from,
+                  :to,
+                  :message
+
     def initialize(config)
       @config = config
-    end
-
-    def from
-      @config[:from]
-    end
-
-    def to
-      @config[:to]
-    end
-
-    def message
-      @config[:message]
+      @from = config[:from]
+      @to = config[:to]
+      @message = config[:message]
     end
 
     def deliver
-      puts "Delivering message from #{from} to #{to} with #{message}"
+      file = rendered_file
+      #Launchy.open("file:///#{URI.parse(URI.escape(messages.first.filepath))}")
+    end
+
+    def template
+      File.read(File.expand_path("../message.html.erb", __FILE__))
+    end
+
+    def rendered_file
+      ERB.new(template).result(binding)
     end
   end
 end
--- notifications_opener.gemspec
+++ notifications_opener.gemspec
@@ -26,10 +26,11 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.11"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "pry"
+  spec.add_development_dependency 'bundler', '~> 1.11'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'pry'
 
-  spec.add_dependency "webmock", ">= 1.0"
-  spec.add_dependency "rack"
+  spec.add_dependency 'webmock', '>= 1.0'
+  spec.add_dependency 'rack'
+  spec.add_dependency 'launchy', '~> 2.2'
 end
